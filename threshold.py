551556ec67f01748b59928b865b1d4181cf46ccb
CtheSky <cthesky@yeah.net>
2017-11-24 23:06:42 +0800

    threshold helper

---
+++ threshold.py
@@ -0,0 +1,128 @@
+import cv2
+import numpy as np
+
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    if orient == 'x':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    else:
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+
+    # 3) Take the absolute value of the derivative or gradient
+    abs_sobel = np.absolute(sobel)
+
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    scaled_sobel = np.uint8(abs_sobel * 255 / np.max(abs_sobel))
+
+    # 5) Create a mask of 1's where the scaled gradient magnitude
+    # is > thresh_min and < thresh_max
+    binary_output = np.zeros_like(scaled_sobel)
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+
+    return binary_output
+
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+
+    # 3) Calculate the magnitude
+    mag = np.sqrt(sobelx ** 2 + sobely ** 2)
+
+    # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scaled_mag = np.uint8(mag * 255 / np.max(mag))
+
+    # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(gray)
+    binary_output[(scaled_mag >= mag_thresh[0]) & (scaled_mag <= mag_thresh[1])] = 1
+
+    return binary_output
+
+
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+
+    # 3) Take the absolute value of the x and y gradients
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    dir_grad = np.arctan2(abs_sobely, abs_sobelx)
+
+    # 5) Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(gray)
+    binary_output[(dir_grad >= thresh[0]) & (dir_grad <= thresh[1])] = 1
+
+    return binary_output
+
+
+def hls_select(img, selected, thresh=(0, 255)):
+    # 1) Convert to HLS color space
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+
+    # 2) Apply a threshold to the selected channel
+    idx = {'h': 0, 'l': 1, 's': 2}[selected.lower()]
+    channel = hls[:, :, idx]
+
+    binary_output = np.zeros_like(channel)
+    binary_output[(channel > thresh[0]) & (channel <= thresh[1])] = 1
+    return binary_output
+
+
+def rgb_select_yellow(img):
+    r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+
+    binary_output = np.zeros_like(r)
+    binary_output[(r > 220) & (g > 220) & (b > 220)] = 1
+    return binary_output
+
+
+def combined_threshold(image):
+    ksize = 5
+
+    # Apply each of the thresh-holding functions
+    gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(50, 255))
+    grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(100, 255))
+    s_channel = hls_select(image, selected='s', thresh=(180, 255))
+    shadowed_yellow = hls_select(image, selected='h', thresh=(18, 63)) & \
+                      hls_select(image, selected='s', thresh=(55, 255)) & \
+                      hls_select(image, selected='l', thresh=(0, 140))
+
+    combined = gradx | grady | s_channel | shadowed_yellow
+    return combined
+
+
+if __name__ == '__main__':
+    import argparse
+    import matplotlib.pyplot as plt
+
+    parser = argparse.ArgumentParser('Display thresh-filtered image.')
+    parser.add_argument('-f', default='test1.jpg', help='name of test image')
+    args = parser.parse_args()
+
+    image = cv2.imread('test_images/{}'.format(args.f))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = combined_threshold(image)
+
+    # Plot the result
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    f.tight_layout()
+    ax1.imshow(image)
+    ax1.set_title('Original Image', fontsize=50)
+    ax2.imshow(result, cmap='gray')
+    ax2.set_title('Threshold Image', fontsize=40)
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    plt.show()
